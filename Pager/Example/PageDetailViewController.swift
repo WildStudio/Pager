3d5a9525ca9112de6ad3d3440dd600f02a111cb2
Christian Aranda <christian@wearemobilefirst.com>
2019-02-20 21:14:49 +0100

    Add example app assets

--- Pager/Example/PageDetailViewController.swift
+++ Pager/Example/PageDetailViewController.swift
@@ -11,34 +11,49 @@ import Pager
 
 
 final class PageDetailViewController: UIViewController {
-
+    
     private static let storyboard = "Demo"
-
+    
     @IBOutlet private var imageView: UIImageView?
     @IBOutlet private var titleLabel: UILabel?
     @IBOutlet private var subtitleLabel: UILabel?
     var descriptionText: String?
-
+    var titleText: String?
+    var imageName: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let text = descriptionText else { return }
-        titleLabel?.text = text
+        guard let description = descriptionText,
+            let title = titleText,
+            let name = imageName else { return }
+        
+        titleLabel?.text = title
+        subtitleLabel?.text = description
+        imageView?.image = UIImage(named: name)
     }
-
+    
     static func instantiate() -> PageDetailViewController? {
         let storyboard = UIStoryboard(name: PageDetailViewController.storyboard, bundle: nil)
-
+        
         guard let viewController = storyboard.instantiateInitialViewController() as? PageDetailViewController else {
             return nil
         }
-
+        
         return viewController
     }
 }
 
 extension PageDetailViewController: ItemPresentable {
     func configuredWith(item: Item) -> UIViewController {
-        descriptionText = item.description
+        guard let model = item as? Model else {
+            descriptionText = item.description
+            return self
+        }
+        
+        titleText = model.title
+        descriptionText = model.description
+        imageName = model.image
+        
         return self
     }
 }
--- PagerExampleApp/PagerExampleApp/ViewController.swift
+++ PagerExampleApp/PagerExampleApp/ViewController.swift
@@ -9,22 +9,31 @@
 import UIKit
 import Pager
 
+class Model: Item {
+    let image: String
+
+    init(title: String, description: String, imageName: String) {
+        self.image = imageName
+        super.init(title: title, description: description)
+    }
+}
+
 class ViewController: UIViewController {
 
-    let items = [Item(title: "Air Jordan 1", description: "Air Jordan 1"),
-                 Item(title: "Air Jordan 2", description: "Air Jordan 2"),
-                 Item(title: "Air Jordan 3", description: "Air Jordan 3"),
-                 Item(title: "Air Jordan 4", description: "Air Jordan 4"),
-                 Item(title: "Air Jordan 5", description: "Air Jordan 5"),
-                 Item(title: "Air Jordan 6", description: "Air Jordan 6"),
-                 Item(title: "Air Jordan 7", description: "Air Jordan 7")]
+    let dataSource = [Model(title: "Air Jordan I", description: "The original. The icon. When you say Air Jordan, this is the sneaker people think of. It still looks fresh more than 30 years later, and it's hard to imagine the popularity of the first Jordan fading anytime soon.", imageName: "I"),
+                      Model(title: "Air Jordan III", description: "This was the first sneaker to sport the famed Jumpman logo, and Jordan Brand might not exist today without it. It looks good, it feels good. It's basically perfect.", imageName: "III"),
+                      Model(title: "Air Jordan XI", description: "There's a reason this is the retro edition Nike puts out every year at Christmas. This is the shoe everyone wants on their feet, and it's been that way since the moment Boyz II Men rocked them at the 1996 Grammys.", imageName: "XI"),
+                      Model(title: "Air Jordan IV", description: "This is the sneaker featured in the legendary scene in Do The Right Thing -- the movie that led to Spike Lee becoming Jordan's right-hand man in an unforgettable series of ads.", imageName: "IV"),
+                      Model(title: "Air Jordan XII", description: "The sneaker that took the Jordan line from a Nike product to its own brand has held up remarkably well through the years, thanks in large part to its association with Michael's epic Flu Game.", imageName: "XII"),
+                      Model(title: "Air Jordan V", description: "As a kid, I remember these more for Will Smith wearing them on The Fresh Prince of Bel-Air than for Michael wearing them on the court. Either way, they looked damn good.", imageName: "V"),
+                      Model(title: "Air Jordan VII", description: "These were the shoes on Michael's feet when he played with the Dream Team in the 1992 Olympics. That itself would be worthy of a high ranking, but they also look and feel really good", imageName: "VII")]
 
     @IBAction func didTapStartButton(_ sender: Any) {
         let controller = Pager.PageContainerViewController.instantiate()
         present(controller, animated: true)
 
-        let controllers = items.map { _ in PageDetailViewController.instantiate() ?? PageDetailViewController() }
-        controller.configure(with: items, controllers: controllers)
+        let controllers = dataSource.map { _ in PageDetailViewController.instantiate() ?? PageDetailViewController() }
+        controller.configure(with: dataSource, controllers: controllers)
     }
 }
 
